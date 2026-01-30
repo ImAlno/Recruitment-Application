796382a9f51a5e541ee8dd9979c635ce4cc50ba5
Gustav Moritz <gmoritz@kth.se>
2026-01-30 15:13:21 +0100

    feat: controller class added, no methods yet

--- backend/src/controller/Controller.ts
+++ backend/src/controller/Controller.ts
@@ -1,11 +1,34 @@
-import { Request, Response } from "express";
-import { fetchUsers } from "../util/UserService";
+'use strict';
 
-export const getUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await fetchUsers();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ error: "Internal server error" });
+//const Validators = require('../util/Validators');  // ? fix later if implemented
+import { DAO } from 'integration/DAO';
+import { ApplicantDTO } from 'model/ApplicantDTO';
+
+/**
+ * The application's controller. No other class shall call the model or
+ * integration layer.
+ */
+export class Controller {
+  /**
+   * Creates a new instance.
+   */
+  protected dao: DAO;
+  protected transactionManager: DAO.getTransactionManager; // TODO Maybe need to change to a type later??
+  constructor() {
+    this.dao = new DAO();
+    this.transactionManager = this.dao.getTransactionMgr();
   }
-};
+
+  /**
+   * Instantiates a new Controller object.
+   *
+   * @return {Controller} The newly created controller.
+   */
+  static async createController() {
+    const contr = new Controller();
+    await contr.dao.createTables(); // TODO Not sure how we will handle table creation
+    return contr;
+  }
+
+}
+export default Controller;
