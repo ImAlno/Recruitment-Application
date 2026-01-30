97557c547c7a3ec4e36ecf8638ae38b65622531f
Gustav Moritz <gmoritz@kth.se>
2026-01-30 15:19:50 +0100

    fix: controller typescript typo

--- backend/src/controller/Controller.ts
+++ backend/src/controller/Controller.ts
@@ -3,6 +3,7 @@
 //const Validators = require('../util/Validators');  // ? fix later if implemented
 import { DAO } from 'integration/DAO';
 import { ApplicantDTO } from 'model/ApplicantDTO';
+import { promises } from 'node:dns';
 
 /**
  * The application's controller. No other class shall call the model or
@@ -24,7 +25,7 @@ export class Controller {
    *
    * @return {Controller} The newly created controller.
    */
-  static async createController() {
+  static async createController(): Promise<Controller> {
     const contr = new Controller();
     await contr.dao.createTables(); // TODO Not sure how we will handle table creation
     return contr;
