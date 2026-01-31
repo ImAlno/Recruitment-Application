279697452a5bfc2b38795de266e4e775230a96cd
Gustav Moritz <gmoritz@kth.se>
2026-01-31 13:13:33 +0100

    feat: drizzle config files and initial schema

---
+++ backend/src/model/ApplicantDTO.ts
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+* DTO for a applicant of the recruitment platform 
+*/
+class ApplicantDTO {
+    /**
+    * Creates a new instance.
+    * @param {} // TODO fill out params matching the Applicant schema in schema.ts
+    */
+    constructor() {
+
+    }
+}
+
+export default ApplicantDTO;
