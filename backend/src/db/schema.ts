297993d4d171fede8a7d2011798e271cb6812d7a
Gustav Moritz <gmoritz@kth.se>
2026-01-31 13:40:20 +0100

    feat: applicant database schema and dto object

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,11 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("applicants", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const applicantsTable = pgTable("applicants", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    firstName: varchar({ length: 255 }).notNull(),
+    lastName: varchar({ length: 255 }).notNull(),
+    username: varchar({ length: 255 }).notNull(),
+    password: varchar({ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    personNumber: varchar({ length: 255 }).notNull().unique(),
 });
--- backend/src/model/ApplicantDTO.ts
+++ backend/src/model/ApplicantDTO.ts
@@ -6,10 +6,32 @@
 class ApplicantDTO {
     /**
     * Creates a new instance.
-    * @param {} // TODO fill out params matching the Applicant schema in schema.ts
-    */
-    constructor() {
+    * @param {number} id The database id PK
+    * @param {string} firstName Applicants first name
+    * @param {string} lastName Applicants last name
+    * @param {string} username Applicants username
+    * @param {string} password Applicants password
+    * @param {string} email Applicants email
+    * @param {string} personNumber Applicants person number
 
+    * This is a dto = immutable object = use readonly
+    */
+    constructor(
+        public readonly id: number,
+        public readonly firstName: string,
+        public readonly lastName: string,
+        public readonly username: string,
+        public readonly password: string,
+        public readonly email: string,
+        public readonly personNumber: string,
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.personNumber = personNumber;
     }
 }
 
