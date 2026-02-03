a8e2329332b970dad45de8d8cfff8b1e9adc3940
Roholla <roholla.jafari@outlook.com>
2026-02-03 09:01:04 +0100

    updated config files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import loader from "./api";
+import loader from "./api/index";
 import dotenv from "dotenv";
 
 // Load environment variables immediately
