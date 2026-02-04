eb3524850f4ffc00c28569f210f91f9aa59acbfa
Gustav Moritz <gmoritz@kth.se>
2026-02-04 10:32:46 +0100

    fix: added seeding of competence table to seed.ts as this table shall have 3 constant entries and wont be modified

--- backend/src/db/seed.ts
+++ backend/src/db/seed.ts
@@ -1,9 +1,9 @@
 import db from "./index";
-import { roleTable } from "./schema";
+import { roleTable, competenceTable } from "./schema";
 
 /** // TODO: This is a temporary function, a better solution should be implemented.
- * The database requires that the role table as an entry for both applicant and recruiter. 
- * This function seeds the database with these roles if they don't already exist.
+ * The database requires that the role table and compentence table has entries for both 1. applicant and recruiter and 2. ticket sales, lotteries and roller coaster operation. 
+ * This function seeds the database with these entries if they don't already exist.
  * You can run this function once to seed the database with npm run db:seed (see package.json for more info). 
  */
 async function seed() {
@@ -14,13 +14,24 @@ async function seed() {
         const existingRoles = await db.select().from(roleTable);
         if (existingRoles.length > 0) {
             console.log("Roles already exist, skipping seed.");
-            return;
-        }
-
-        await db.insert(roleTable).values([
+        } else {
+            await db.insert(roleTable).values([
             { name: "recruiter" },
             { name: "applicant" },
-        ]);
+            ]);
+        }
+
+        const existingCompetence = await db.select().from(competenceTable);
+        if (existingCompetence.length > 0) {
+            console.log("Competence entries already exist, skipping seed.");
+            return;
+        } else {
+            await db.insert(competenceTable).values([
+                { name: "ticket sales" },
+                { name: "lotteries" },
+                { name: "roller coaster operation" }, 
+            ]);
+        }
 
         console.log("Seeding completed successfully!");
     } catch (error) {
